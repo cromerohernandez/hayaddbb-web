3a05f35cb80de6ea84eb43630b374a789c7f300f
cromerohernandez <c.romero.hsv@gmail.com>
2021-09-24 18:45:58 +0200

    ADD: HayaDBService

---
+++ src/services/HayaDBService.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+const http = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  withCredentials:true
+})
+
+http.interceptors.response.use(
+  response => response.data,
+  error => {
+    if (error.response && error.response.status === 401) {
+      localStorage.clear()
+      window.location.assign('/')
+    }
+    return Promise.reject(error)
+  }
+)
+
+const houseDetail = (houseId) => http.get(`/houses/${taskId}`)
+
+const TaskbleService = {
+  houseDetail
+}
+
+export default TaskbleService
